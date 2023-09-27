7fd204ec7efd71790f5dc8041438185ef64e9113
Vitor <vitornascimento6650@gmail.com>
2023-09-26 21:01:29 -0300

    Implementando background e navbar para a página home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,46 @@
 <template>
-  <div class="w-full h-full">
-    <div class="w-full h-20">
-      <nav class="bg-slate-950 text-white">
-        <div class="flex justify-center ">
-          <img class="w-16 mt-2 mb-2 transition duration-700 hover:scale-110" :src="logoSpider" alt="logo homem aranha spiderverse">
+  <div :style="{ backgroundImage: 'url(' + bgSpiderHome + ')' }" class="bg-config flex">
+
+    <div class="flex w-full h-full">
+      <nav class="bg-neutral-950 text-white flex flex-col justify-center h-full w-36">
+        <div class="flex flex-col items-center">
+          <p class="mb-8">Aranhas criadas</p>
+          <p class="mb-20">Início</p>
+        </div>
+
+        <div class="flex flex-col items-center">
+          <img class="w-12 transition duration-700 hover:scale-110" :src="logoSpider" alt="logo homem aranha spiderverse">
+        </div>
+
+        <div class="flex flex-col items-center">
+          <p class="mt-20">Criar aranhas</p>
+          <p class="mt-8">Descobrir aranha</p>
         </div>
       </nav>
+
+      <div class="w-full mr-5 mt-3 flex justify-end text-white">
+        <p>perfil</p>
+      </div>
     </div>
+    
   </div>
 </template>
 
 <script>
 import logoSpider from '@/assets/img/login/logoSpider.png';
+import bgSpiderHome from '@/assets/img/home/bg-spiderverse-home.jpg';
 
 export default {
   name: "Home",
   data: () => ({
-    logoSpider: logoSpider
+    logoSpider: logoSpider,
+    bgSpiderHome: bgSpiderHome,
   }),
 }
 </script>
 
 <style scoped>
-  
+.bg-config {
+  background-size: cover;
+}
 </style>
